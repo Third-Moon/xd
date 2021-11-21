2c3c06c3ab56f39dcd69d9e75adba561d32d4bfd
asdasdka <79456577+asdasdka@users.noreply.github.com>
2021-11-21 18:10:03 +0100

    sadasd

---
+++ pelda.js
@@ -0,0 +1,22 @@
+function peldaProgram(){
+    const szam = parseInt(prompt("Írj be egy pozitív egész számot, megmondom prím-e."));
+    let prim = true;
+
+if (szam === 1) {
+    document.getElementById("gomb").innerHTML("Az 1 az nem prím szám.");
+}
+
+else if (szam > 1) {
+    for (let i = 2; i < szam; i++) {
+        if (szam % i == 0) {
+            prim = false;
+            break;
+        }
+    }
+    if (prim) {
+        document.getElementById("gomb").innerHTML("A" + szam + "egy prím szám.");
+    } else {
+        document.getElementById("gomb").innerHTML("A" + szam + "nem egy prím szám.");
+    }
+}
+}
